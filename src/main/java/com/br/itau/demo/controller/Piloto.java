4ed9813e4f687ac380c1a0f47257ed358673c96f
zefor2 <zefor2@gmail.com>
2019-03-20 23:52:11 -0300

    correcoes

--- src/main/java/com/br/itau/demo/controller/Piloto.java
+++ src/main/java/com/br/itau/demo/controller/Piloto.java
@@ -7,31 +7,46 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.br.itau.demo.config.CpfsConfig;
 
+import reactor.core.CoreSubscriber;
+import reactor.core.publisher.Mono;
+
 @RestController
 public class Piloto {
 	
 	@Autowired
 	CpfsConfig cpfs;
 	
-	@GetMapping("/v1/{id}")
-	public boolean getPiloto(@PathVariable Integer id) {
-		long tempoInicio = System.nanoTime();
- 
-		boolean bool = cpfs
-		.getColecao()
-		.contains(id);
-
-		System.out.println(System.nanoTime()-tempoInicio);
-		
-		long tempoInicio2 = System.nanoTime();
-		 
-		boolean bool2 = cpfs
-		.getColecao2()
-		.contains(id);
-
-		System.out.println(System.nanoTime()-tempoInicio2);
 	
-		return bool;
+	
+	@GetMapping("/v1/{id}")
+	public Mono<Boolean> getPiloto(@PathVariable Integer id) {
+		
+ 
+		
+		Mono<Boolean> mono =  new Mono<Boolean>() {
+			
+			@Override
+			public void subscribe(CoreSubscriber<? super Boolean> arg0) {
+				long tempoInicio = System.nanoTime();
+				boolean bool = cpfs
+						.getColecao()
+						.contains(id);
+				System.out.println(System.nanoTime()-tempoInicio);
+				
+				long tempoInicio2 = System.nanoTime();
+				 
+				
+				Boolean b = new Boolean(bool);
+				boolean bool2 = cpfs
+						.getColecao2()
+						.contains(id);
+
+						System.out.println(System.nanoTime()-tempoInicio2);
+				
+			}
+		};
+		
+		return mono.doOnNext(Consumer<Boolean> -> Boolean b = true );
 	}
 
 }
